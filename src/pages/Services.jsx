30fa5315f9db5d68d9b8ae9d98847aa0a4f7a421
fabiotoledo12 <fvtoledojf@hotmail.com>
2024-09-09 10:47:09 -0300

    EFETUADO O UPDATE, NÃO ESTÁ 100%

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -112,22 +112,39 @@ const Input = styled.input`
   border-radius: 4px;
 `;
 
-
-
 const Services = () => {
 
+    const [formData, setFormData] = useState({
+        service: '',
+        date: '',
+        // reserva: true
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+      };
+
     const [open, setOpen] = React.useState(false);
+    
     const handleClickOpen = () => {
         setOpen(true);
     };
     const handleClose = () => {
-        setOpen(false);
+        if (formData.service === "" || formData.date === ""){
+            toast.warning("🤔 Você precisa preecher obrigatoriamente todos os campos 🤔.");
+        } else {
+            setOpen(false);
+        }
+        
     };
-
 
     const [loading, setLoading] = useState(false);
     const [clientes, setClientes] = useState([]);
-    const [date, setDate] = useState();
+    // const [date, setDate] = useState();
 
     useEffect(() => {
         getClientes();
@@ -149,28 +166,47 @@ const Services = () => {
 
     //TODO Aqui ainda está com erro, está dando update somente no ultimo registro
     const  handleUpdate = async (id) => { 
-        const bookingUpdate = clientes.filter((bookingItem) => bookingItem._id === id);
-        
-        console.log( id )
-        console.log( bookingUpdate[0].date )
+        // handleClickOpen()
 
-        if (bookingUpdate[0]._id === id ){
+        const bookingUpdate = clientes.filter((bookingItem) => bookingItem._id === id);
+        // console.log("teste dentro const booking: " + bookingUpdate[0]._id )  
+        console.log( id )
+
+        const { data } = await axios.get(`http://localhost:3001/booking`)
+        const bookingUpdateSearch = data.filter((bookingItem) => bookingItem._id === id);
         
-            console.log( date )
-            await axios.put(`http://localhost:3001/booking/${id}`, {
-                date: date
-                // service: 'Personal', 
-            })
+
+        if(bookingUpdateSearch[0]._id === id ){
+            // console.log(bookingUpdate[0].service)
+            // formData.service = bookingUpdateSearch[0].service
+            // formData.date = bookingUpdateSearch[0].date
+
+            // biome-ignore lint/style/useConst: <explanation>
+            let service = formData.service
+            // biome-ignore lint/style/useConst: <explanation>
+            let date = formData.date
+            
+            if (bookingUpdate[0]._id === id ){                   
+
+                await axios.put(`http://localhost:3001/booking/${id}`, {
+                    service: service,
+                    date: date,
+                })
+
+                console.log(service)
+                console.log(date)
+                console.log(id)
+            }
             getClientes()
         }
+        
         // console.log(bookingUpdate)
-        // alert("cheguei")
     }
 
     const handleUpdateCancel = async (id) => { 
         const bookingUpdate = clientes.filter((bookingItem) => bookingItem._id === id);
         if (bookingUpdate[0]._id === id ){
-        console.log( bookingUpdate )
+            // console.log( bookingUpdate )
             await axios.put(`http://localhost:3001/booking/${id}`, {
                 reserva: false,
             })
@@ -221,10 +257,66 @@ const Services = () => {
                             ) : (
                                 clientes?.length > 0 ?
                                     clientes.map((cliente, index) => (                                      
+                                        
                                         <ContentTableTr
                                             key={index}
                                             style={{background: index % 2 === 0 ? 'rgba(0,0,0,.1)' : 'unset'}}
-                                        >
+                                        >                                    
+
+
+
+{/* -----------------------------------Modal-----------------------------} */}
+<Dialog
+    key={index}
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+>
+        <DialogTitle id="alert-dialog-title">
+        {"Alterar Meu Agendamento"}
+        </DialogTitle>
+        <DialogContent>
+
+        <FormGroup>
+            <Label htmlFor="service">Serviço</Label>
+            <Select id="service" name="service" value={formData.service} onChange={handleChange}>
+                <option value="">Selecione um serviço</option>
+                <option value="Academia">Academia</option>
+                <option value="Personal">Personal Trainer</option>
+                <option value="Consulta">Consulta de Saúde</option>
+            </Select>
+            </FormGroup>
+            <FormGroup>
+            <Label htmlFor="date">Data</Label>
+            <Input type="datetime-local" id="date" name="date" value={ formData.date } onChange={handleChange} />
+        </FormGroup>
+            
+            {/* <FormGroup>
+                <Select id="service" name="service" onChange={""}>
+                    <option value="">Selecione um serviço</option>
+                    <option value="Academia">Academia</option>
+                    <option value="Personal">Personal Trainer</option>
+                    <option value="Consulta">Consulta de Saúde</option>
+                </Select>
+                <Label htmlFor="date">Data</Label>
+                <Input type="datetime-local" id="date" name="date"
+                    value={date}
+                    onChange={(ev) => setDate(ev.target.value)}
+                />
+            </FormGroup> */}
+
+        </DialogContent>
+        <DialogActions>
+            {/* <Button type= "button" onClick={ ( ) => handleUpdate( cliente._id  ) }  >Alterar</Button> */}
+            <Button type= "button" onClick={ handleClose }  >Alterar</Button>
+            <Button type= "button" onClick={ handleClose } autoFocus>Cancelar</Button>
+        </DialogActions>
+</Dialog>
+
+{/* -----------------------------------Modal-----------------------------} */}
+
+                                            {/* <ContentTableTd>{cliente._id}</ContentTableTd> */}
                                             <ContentTableTd>{cliente.service}</ContentTableTd>
                                             <ContentTableTd> 
                                                 {/* {cliente.date}*/}
@@ -247,41 +339,9 @@ const Services = () => {
                                             <ContentTableTd>
                                                 {/* <Button type="button" onClick={ () => handleUpdate(cliente._id) } > */}
                                                 
-                                                {/* -----------------------------------Modal-----------------------------} */}
-                                                <Dialog
-                                                        open={open}
-                                                        onClose={handleClose}
-                                                        aria-labelledby="alert-dialog-title"
-                                                        aria-describedby="alert-dialog-description"
-                                                    >
-                                                        <DialogTitle id="alert-dialog-title">
-                                                        {"Alterar Meu Agendamento"}
-                                                        </DialogTitle>
-                                                        <DialogContent>
-
-                                                            <FormGroup>
-                                                                <Select id="service" name="service" onChange={""}>
-                                                                    <option value="">Selecione um serviço</option>
-                                                                    <option value="Academia">Academia</option>
-                                                                    <option value="Personal">Personal Trainer</option>
-                                                                    <option value="Consulta">Consulta de Saúde</option>
-                                                                </Select>
-                                                                <Label htmlFor="date">Data</Label>
-                                                                <Input type="datetime-local" id="date" name="date"
-                                                                    value={date}
-                                                                    onChange={(ev) => setDate(ev.target.value)}
-                                                                />
-                                                            </FormGroup>
-
-                                                        </DialogContent>
-                                                        <DialogActions>
-                                                            <Button onClick={ () => handleUpdate(cliente._id) }  >Alterar</Button>
-                                                            <Button onClick={handleClose} autoFocus>Cancelar</Button>
-                                                        </DialogActions>
-                                                </Dialog>
-                                                {/* -----------------------------------Modal-----------------------------} */}
-                                                
-                                                <Button type="button" onClick={ handleClickOpen } >
+                                                <Button type="button" onClick={ () => handleUpdate( cliente._id ) }  >
+                                                {/* <Button type="button" onClick={ () => handleClickOpen() } >                                                   */}
+                                                    
                                                     <EventAvailable />
 
                                                     { /* Modal */ }
